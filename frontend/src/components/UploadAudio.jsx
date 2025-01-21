ab2b18ecc44ae2ccbedec9117f592d71b359e3ec
KeyberSequera8 <16-11120@usb.ve>
2025-01-21 18:19:31 -0400

    RTT-19 Se agrega función para la selección de archivos
    Se agrega función para manejar el seleccionado de archivos, falta por implementar handleUpload para controlar el comportamiento en la subida de archivos

--- frontend/src/components/UploadAudio.jsx
+++ frontend/src/components/UploadAudio.jsx
@@ -2,8 +2,19 @@ import React, { useState } from 'react';
 
 // Componente UploadAudio para manejar la selección y carga de archivos de audio:
 const UploadAudio = () => {
+  
   // Estado para almacenar el archivo seleccionado:
   const [file, setFile] = useState(null);
+  
   // Estado para mostrar si el archivo se está cargando:
   const [uploading, setUploading] = useState(false);
+
+  // Función que se activa cuando se selecciona un archivo:
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile); // Guardamos el archivo en el estado
+    if (selectedFile) {
+      handleUpload(selectedFile); // Subimos el archivo automáticamente
+    }
+  };
 };
