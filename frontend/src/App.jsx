e46f316d2ee0d1c28847d40b6f13ddde186bbd90
KeyberSequera8 <16-11120@usb.ve>
2025-01-21 20:07:31 -0400

    RTT-19 Se agrega el componente para subir archivos de audio a la interfaz de usuario
    Se agrega componente para subir archivos de audio a la aplicación principal del FrontEnd.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <>
       <h1>BugBuster Project</h1>
+      <div className="UploadAudio">
+        {/* Se agrega el campo para subir archivos de audio */}
+        <UploadAudio /> {/* Mostramos el componente para cargar archivos */}
+      </div>
       <PingComponent />
     </>
   );
