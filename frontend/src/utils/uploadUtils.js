c160d349db9e9340b160a14f0fb6cadaec72d3ac
Sergio <sergiocarrillo_@outlook.com>
2025-01-29 21:34:22 -0400

    RTT-41 se ha agregado el param, para pasarle el componente origen del mensaje

--- frontend/src/utils/uploadUtils.js
+++ frontend/src/utils/uploadUtils.js
@@ -1,21 +1,23 @@
 import { apiService } from '../service/api';
 import { setError, setSuccess } from '../store/slices/errorSlice';
+import { getMessage } from '../utils/localeHelper';
 
 /**
  * Función para manejar la carga de archivos.
  * @param {File} file - El archivo a cargar.
  * @param {string} endpoint - El endpoint de la API donde se enviará el archivo.
  * @param {Function} dispatch - La función dispatch de Redux para manejar mensajes.
+ * @param {string} origin - Propiedad que identifica el componente origen del mensaje
  */
-export const handleFileUpload = async (file, endpoint, dispatch) => {
+export const handleFileUpload = async (file, endpoint, dispatch, origin) => {
   const formData = new FormData();
   formData.append('uploadedAudio', file, file.name);
 
   try {
     await apiService.post(endpoint, formData);
-    dispatch(setSuccess({ message: 'Archivo de audio guardado en el servidor.' }));
+    dispatch(setSuccess({ message: getMessage("UploadAudio", "success"), origin: origin }));
   } catch (error) {
-    dispatch(setError({ message: error.message }));
+    dispatch(setError({ message: getMessage("UploadAudio", "error", { error: error.message }), origin: origin }));
     throw error; // Re-lanza el error en caso de necesitar manejarlo
   }
 };
