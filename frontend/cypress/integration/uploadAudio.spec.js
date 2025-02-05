f80b092571a487df9313a81d1c38df3c66334e9c
Sergio <sergiocarrillo_@outlook.com>
2025-02-05 10:01:26 -0400

    RTT-50 se ha actualizado la prueba para trabajar con variables de entorno

--- frontend/cypress/integration/uploadAudio.spec.js
+++ frontend/cypress/integration/uploadAudio.spec.js
@@ -1,18 +1,19 @@
 describe('Subir, almacenar y validar que el archivo almacenado en la bd es una copia del archivo original', () => {
   it('debería subir un archivo, y compara el codificado base64 del archivo original con el almacenado en la base de datos', () => {
     const filePath = 'cypress/fixtures/test-file.mp3';  // Ruta al archivo local de prueba
+    const host = Cypress.env('HOST');
 
     // Interceptamos la respuesta del POST para capturar el resultado
     cy.intercept('POST', '/api/audio').as('audioUpload');
 
     // Visitar la página donde está el formulario de carga
-    cy.visit('http://localhost:3000');
+    cy.visit(host);
 
     // Simulamos la carga del archivo, buscamos el elemento con id upload-audio
     cy.get('#upload-audio').attachFile('test-file.mp3');
 
     // Esperamos la respuesta del backend
-    cy.wait('@audioUpload').then((interception) => {
+    cy.wait('@audioUpload', {timeout: 60000}).then((interception) => {
       const response = interception.response.body;
 
       // Decodificar base64 recibido del backend
