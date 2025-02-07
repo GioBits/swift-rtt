e39b728e2b52309944c996f5ffb1405c3c298dc3
Sergio <sergiocarrillo_@outlook.com>
2025-02-06 22:01:35 -0400

    RTT-61 se ha integrado un contador creciente y un indicador de grabación

--- frontend/src/components/RecordAudio.jsx
+++ frontend/src/components/RecordAudio.jsx
@@ -1,22 +1,42 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import { useDispatch } from 'react-redux';
 import { handleFileUpload } from '../utils/uploadUtils';
 import { clearError } from '../store/slices/errorSlice';
 import { useAudioRecorder } from '../hooks/useAudioRecorder';
-import { FFmpeg } from '@ffmpeg/ffmpeg'
+import { FFmpeg } from '@ffmpeg/ffmpeg';
 import { convertWavToMp3 } from '../utils/audioUtils';
 import { TranslationContext } from '../contexts/TranslationContext';
-
+import '../styles.css';
 
 const RecordAudio = () => {
   const { isRecording, setUploading } = useContext(TranslationContext);
   const dispatch = useDispatch();
   const ffmpeg = new FFmpeg();
+  const [elapsedTime, setElapsedTime] = useState(0);
 
   useEffect(() => {
     dispatch(clearError());
   }, [dispatch, isRecording]);
+
+  useEffect(() => {
+    let timer;
+    if (isRecording) {
+      setElapsedTime(0);
+      timer = setInterval(() => {
+        setElapsedTime(prevTime => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+    return () => clearInterval(timer);
+  }, [isRecording]);
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+  };
 
   const uploadAudio = async (audioBlob) => {
     setUploading(true);
@@ -32,10 +52,12 @@ const RecordAudio = () => {
   const { startRecording, stopRecording } = useAudioRecorder(dispatch, uploadAudio);
 
   return (
-    <div>
-      <Button
-        variant="contained"
-        onClick={isRecording ? stopRecording : startRecording}>
+    <div className="record-container">
+      <div className="record-status">
+        {isRecording && <div className="blinking-circle"></div>}
+        <span className="timer">{formatTime(elapsedTime)}</span>
+      </div>
+      <Button variant="contained" onClick={isRecording ? stopRecording : startRecording}>
         {isRecording ? "Detener Grabación" : "Iniciar Grabación"}
       </Button>
     </div>
