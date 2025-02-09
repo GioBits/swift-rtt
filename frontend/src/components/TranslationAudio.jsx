936e447652850f76f19a30c5b263f455934a1b7a
abelzavaleta <abel@origin.63>
2025-02-09 04:56:33 -0400

    RTT-56 feat<frontend>: change on css translation audio component

--- frontend/src/components/TranslationAudio.jsx
+++ frontend/src/components/TranslationAudio.jsx
@@ -1,26 +1,28 @@
 import { TranslationContext } from '../contexts/TranslationContext';
 import { useContext } from 'react';
 import { useSelector } from 'react-redux';
+import "../styles.css"
 
 const TranslationAudio = () => {
   const { audioUrl, uploading } = useContext(TranslationContext);
   const { message, type, origin } = useSelector(state => state.error);
 
+  let validateOrigin = origin === "UploadAudio" || origin === "RecordAudio";
+  let color = type === "error" ? "red" : "green";
+
+
   return (
-    <div className='trascripcion-audio'>
+    <div className="trasncription-audio">
       {audioUrl && (
-        <div style={{ marginTop: 20 }}>
+        <div>
           <audio controls src={audioUrl}></audio>
         </div>
       )}
       {uploading && <p>Cargando archivo...</p>}
-      {type === 'error' && message && (origin === "UploadAudio" || origin === "RecordAudio") && (
-        <div style={{ color: 'red', marginTop: 20 }}>
+      {type && message && validateOrigin && (
+        <div style={{ color: color }}>
           {message}
         </div>
-      )}
-      {type === "success" && message && (origin === "UploadAudio" || origin === "RecordAudio") && (
-        <div style={{ color: "green", marginTop: 20 }}>{message}</div>
       )}
     </div>
   )
