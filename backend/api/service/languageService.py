5a5e6349064d6205b84bfa9abefb41f0b5df1606
abelzavaleta <abel@origin.63>
2025-02-11 02:07:46 -0400

    RTT-78 feat<backend>: controller for language funcionalities

--- backend/api/service/languageService.py
+++ backend/api/service/languageService.py
@@ -4,6 +4,16 @@ from models.languages import Language, LanguageSchema
 from fastapi import HTTPException
 
 def get_all_languages():
+    """
+    Retrieve all language records from the database.
+
+    This function creates a new database session, queries all records from the
+    Language table, converts them to LanguageSchema objects, and returns them
+    as a list. The database session is closed after the operation.
+
+    Returns:
+        list: A list of LanguageSchema objects representing all languages in the database.
+    """
     # Create a database session
     db = SessionLocal()
     try:
@@ -13,6 +23,15 @@ def get_all_languages():
         db.close()
 
 def create_language(code: str, name: str):
+    """
+    Create a new language entry in the database.
+    Args:
+        code (str): The unique code representing the language.
+        name (str): The name of the language.
+    Returns:
+        LanguageSchema: The newly created language object if successful.
+        None: If the language with the given code already exists.
+    """
     # Create a database session
     db = SessionLocal()
     try:
