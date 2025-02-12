dd35acb1ca3157dcaf979b6b8b82ed52bf0d22ea
abelzavaleta <abel@origin.63>
2025-02-12 18:37:48 -0400

    RTT-65 feat<backend>: move transcriber and translate to utilsRoute

--- backend/api/routes/utils.Route.py
+++ backend/api/routes/utils.Route.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, UploadFile, File, Form
 from fastapi.responses import JSONResponse
 from utils.transcribe import transcriber
+from utils.translate import translate
 import tempfile
 import os
 import asyncio
@@ -28,3 +29,24 @@ async def transcribe_audio_file(file: UploadFile = File(...), language: str = Fo
         if os.path.exists(temp_path):
             os.unlink(temp_path)
         raise HTTPException(status_code=500, detail=f"Error en transcripción: {str(e)}")
+
+@router.post("/translate")
+async def translate_file(file: UploadFile = File(...)):
+    temp_path = None 
+    
+    try:
+        content = await file.read()
+        text = content.decode("utf-8")
+        
+        loop = asyncio.get_running_loop()
+        translated_text = await loop.run_in_executor(
+            None, 
+            lambda: translate.translate_text(text)
+        )
+        
+        return JSONResponse(content={"traduccion": translated_text})
+        
+    except Exception as e:
+        if temp_path and os.path.exists(temp_path):
+            os.unlink(temp_path)
+        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
