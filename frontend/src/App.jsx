7834c33279ece15d84b18bb76e66532bd2f4b393
Sergio Carrillo <sergiocarrillo_@outlook.com>
2025-02-13 02:31:30 -0400

    RTT-83 new components integrated.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,31 @@
-import "./App.css";
-import PingComponent from "./components/PingComponent";
 import { MediaProvider } from "./contexts/MediaProvider";
-import TranslationAudio from "./components/TranslationAudio";
 import LanguageSelector from "./components/LanguageSelector";
 import MediaUpload from "./components/MediaUpload";
+import "./index.css";
+import MediaText from "./components/MediaText";
 
 function App() {
+  const models = [
+    { id: "1", name: "Modelo 1" },
+    { id: "2", name: "Modelo 2" },
+    { id: "3", name: "Modelo 3" },
+  ];
+
   return (
-    <>
-      <MediaProvider>
-        <h1>Real Time Translation</h1>
-        <LanguageSelector />
-        <div className="upload-input">
-          <MediaUpload />
+    <div className="bg-neutral-800 h-screen w-screen flex">
+      <div className="w-[60vw] h-[70vh] flex m-auto">
+        <div className="w-1/2 h-full flex flex-col m-auto p-4">
+          <MediaProvider>
+            <LanguageSelector />
+            <MediaUpload />
+          </MediaProvider>
         </div>
-        <TranslationAudio />
-      </MediaProvider>
-      <PingComponent />
-    </>
+        <div className="w-1/2 h-full flex flex-col m-auto rounded gap-4">
+          <MediaText title="Transcripción" models={models} />
+          <MediaText title="Traducción" models={models} />
+        </div>
+      </div>
+    </div>
   );
 }
 
