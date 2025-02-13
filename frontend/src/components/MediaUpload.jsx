b22d495eff55d76a7132e8cf7594c72e5e071551
Sergio Carrillo <sergiocarrillo_@outlook.com>
2025-02-13 05:09:56 -0400

    RTT-83 the uploading state variable has been integrated to do an upload rendering

--- frontend/src/components/MediaUpload.jsx
+++ frontend/src/components/MediaUpload.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import RecordAudio from './RecordAudio';
 import Dropzone from './Dropzone';
-import { Button } from '@mui/material';
+import { Button, CircularProgress } from '@mui/material';
+import { MediaContext } from '../contexts/MediaContext';
 import '../styles.css';
 
 const MediaUpload = () => {
+  const { uploading } = useContext(MediaContext);
   const [buttonSelected, setButtonSelected] = useState(true);
   const [isClicked, setIsClicked] = useState(true);
 
@@ -19,7 +21,7 @@ const MediaUpload = () => {
     <div className='flex flex-col w-full max-w-[410px] m-auto bg-white mt-0 rounded-b-lg p-10 pt-0'>
       <div className='w-full flex flex-row space-between h-auto m-auto'>
         <Button
-          variant= {isClicked && buttonSelected ? 'contained': 'outlined'}
+          variant={isClicked && buttonSelected ? 'contained' : 'outlined'}
           onClick={() => handleButtonClick(true)}
           sx={{
             textTransform: 'none',
@@ -33,23 +35,25 @@ const MediaUpload = () => {
           Subir archivo
         </Button>
         <Button
-            variant= {isClicked && buttonSelected ? 'outlined': 'contained'}
-            onClick={() => handleButtonClick(false)}
-            sx={{
-              textTransform: 'none',
-              pointerEvents: isClicked && buttonSelected === false ? 'none' : 'auto',
-              width: '50%',
-              height: '50px',
-              margin: '10px',
-              backgroundColor: isClicked && buttonSelected ? 'white' : 'rgb(2 132 199)'
-            }}
-          >
-            Grabar
-          </Button>
+          variant={isClicked && buttonSelected ? 'outlined' : 'contained'}
+          onClick={() => handleButtonClick(false)}
+          sx={{
+            textTransform: 'none',
+            pointerEvents: isClicked && buttonSelected === false ? 'none' : 'auto',
+            width: '50%',
+            height: '50px',
+            margin: '10px',
+            backgroundColor: isClicked && buttonSelected ? 'white' : 'rgb(2 132 199)'
+          }}
+        >
+          Grabar
+        </Button>
       </div>
 
-      <div className='box-border w-full bg-white h-[260px] p-2'>
-        {buttonSelected ? (
+      <div className='box-border w-full bg-white h-[260px] p-2 flex items-center justify-center'>
+        {uploading ? (
+          <CircularProgress />
+        ) : buttonSelected ? (
           <Dropzone />
         ) : (
           <RecordAudio />
