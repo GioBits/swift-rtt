732375b91610792cfd1c2502169edee8ef4b494c
Sergio Carrillo <sergiocarrillo_@outlook.com>
2025-02-22 16:36:57 -0400

    RTT-94 feat: responsive App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </div>
 
         {/* Main Content */}
-        <div className="w-[60vw] h-[80vh] gap-6 lg:flex m-auto block">
+        <div className="w-[90vw] md:w-[70vw] lg:w-[60vw] h-[80vh] gap-6 lg:flex m-auto block">
           <MediaProvider>
             {/* Upload Section */}
             <div className="lg:w-1/2 w-full h-full flex flex-col m-auto bg-white rounded-lg shadow-md">
