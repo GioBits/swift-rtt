df97a7dc057911857ad7c63f4e1b4557aebd3e03
abelzavaleta <abel@origin.63>
2025-02-23 05:45:51 -0400

    RTT-103 feat<frontend>: hide navbar

--- frontend/src/components/NavbarComponent.jsx
+++ frontend/src/components/NavbarComponent.jsx
@@ -8,13 +8,13 @@ function NavbarComponent() {
     <div className='h-[60px] bg-primary flex'>
         <div className='w-[30%] h-full border-r border-gray-500 flex'>
             <div className='w-auto h-[40px] m-auto flex'>
-                <div className='flex' >
+                {/* <div className='flex' >
                     <img
-                    src="/src/assets/logo.png"  // Asegúrate de que el archivo esté en la carpeta public
-                    alt="Logo"
-                    style={{ width: 70, height: 40 }}
+                        src="src/assets/logo.png"  // Asegúrate de que el archivo esté en la carpeta public
+                        alt="Logo"
+                        style={{ width: 70, height: 40 }}
                     />
-                </div>
+                </div> */}
                 <div className='flex'>
                     <div className='text-2xl m-auto font-semibold text-white'>
                         Real Time Translation
@@ -24,7 +24,7 @@ function NavbarComponent() {
         </div>
         <div className='w-[70%] h-full flex'>
             <div className='w-auto h-[40px] m-auto flex ml-[20px]'> 
-                <div className='mr-[10px] flex'>
+                {/* <div className='mr-[10px] flex'>
                     <div className='m-auto text-2xl m-auto font-semibold text-white'>
                         Traducir
                     </div>
@@ -33,7 +33,7 @@ function NavbarComponent() {
                     <div className='m-auto text-2xl m-auto font-semibold text-white'>
                         Historial
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     </div>
