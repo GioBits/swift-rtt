e9db3836562dc0f2472567055184812b7ad45fa4
Sergio Carrillo <sergiocarrillo_@outlook.com>
2025-02-28 05:39:06 -0400

    RTT-105 CSS TAILWIND FIX

--- frontend/src/components/ParticleBg.jsx
+++ frontend/src/components/ParticleBg.jsx
@@ -2,28 +2,18 @@ import { useEffect, useState, useMemo } from 'react'
 import Particles, { initParticlesEngine } from '@tsparticles/react'
 import { loadSlim } from '@tsparticles/slim'
 
-// import { loadFull } from 'tsparticles'
-
 export default function ParticleBg () {
   const [init, setInit] = useState(false)
 
-  // this should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      // await loadAll(engine);
-      // await loadFull(engine);
       await loadSlim(engine)
-      // await loadBasic(engine);
     }).then(() => {
       setInit(true)
     })
   }, [])
 
   const particlesLoaded = () => {
-    // console.log(container)
   }
 
   const options = useMemo(
@@ -31,16 +21,6 @@ export default function ParticleBg () {
       fullscreen: { enable: false, zIndex: 0 },
       fpsLimit: 120,
       interactivity: {
-        // events: {
-        //   onClick: {
-        //     enable: true,
-        //     mode: 'push'
-        //   }
-        //   // onHover: {
-        //   //   enable: true,
-        //   //   mode: 'repulse'
-        //   // }
-        // },
         modes: {
           push: {
             quantity: 4
--- frontend/src/components/mediaUpload/MediaUpload.jsx
+++ frontend/src/components/mediaUpload/MediaUpload.jsx
@@ -24,9 +24,9 @@ const MediaUpload = () => {
   }
 
   return (
-    <div className='box-border flex flex-col h-full w-full mix-w-[400px] m-auto rounded-b-lg'>
+    <div className='box-border flex flex-col h-full w-full mix-w-[400px] m-auto'>
       <MediaUploadSelector {...props} />
-      <div className='box-border w-full bg-white h-[calc(100%-80px)] p-5 flex items-center justify-center'>
+      <div className='box-border w-full h-[calc(100%-80px)] p-5 flex items-center justify-center'>
         {!getUploading ? (
           <CircularProgress />
         ) : buttonSelected ? (
