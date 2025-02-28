f77d9a85188b221012f41212ac3df63c2ffc4318
Sergio Carrillo <sergiocarrillo_@outlook.com>
2025-02-28 14:51:28 -0400

    RTT-105 CSS on Views and update on authSlice implement a mock JWT

--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -1,24 +1,39 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+function decodeJWT(token) {
+  try {
+    const payloadBase64 = token.split(".")[1];
+    const payloadJSON = atob(payloadBase64);
+    return JSON.parse(payloadJSON);
+  } catch (error) {
+    console.error("Error al decodificar el token:", error);
+    return null;
+  }
+}
 
 export const loginUser = createAsyncThunk(
   "auth/loginUser",
   async (credentials, thunkAPI) => {
-    const { user, password } = credentials;
+    const fakeToken =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3NDA3NTkwNTAsImV4cCI6MTc3MjI5NTA1OCwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsImlkIjoiMzQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20ifQ.TleIsUJCWQHyRJpv6xZb25LWmVGGkJtb5n663eILKgs";
 
-    if (user === "admin" && password === "admin") {
-      const fakeToken = "fake-jwt-token-12345";
-      const userData = { name: "Admin", email: "admin@host.com" };
-
-      localStorage.setItem("token", fakeToken);
-      localStorage.setItem("user", JSON.stringify(userData));
-
-      return { token: fakeToken, user: userData };
-    } else {
-      return thunkAPI.rejectWithValue("Credenciales incorrectas");
+    const decoded = decodeJWT(fakeToken);
+    if (!decoded) {
+      return thunkAPI.rejectWithValue("Error al procesar el token");
     }
+
+    const userData = {
+      id: decoded.id,
+      email: decoded.Email,
+    };
+
+    localStorage.setItem("token", fakeToken);
+    localStorage.setItem("user", JSON.stringify(userData));
+
+    return { token: fakeToken, user: userData };
   }
 );
+
 
 const authSlice = createSlice({
   name: "auth",
@@ -48,10 +63,12 @@ const authSlice = createSlice({
       })
       .addCase(loginUser.rejected, (state, action) => {
         console.log("❌ Error en login:", action.payload);
+        state.user = null;
+        state.token = null;
+        state.isAuthenticated = false;
         state.error = action.payload;
       });
   },
-  
 });
 
 export const { logout } = authSlice.actions;
