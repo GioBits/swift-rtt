1ee71554a2e59a2e980ea58430781c4b2904bee2
Sergio Carrillo <sergiocarrillo_@outlook.com>
2025-03-07 00:23:37 -0400

    RTT-142 feat <frontend>: erase registerUserAction

--- frontend/src/store/slices/authActions.js
+++ frontend/src/store/slices/authActions.js
@@ -29,28 +29,3 @@ export const loginUser = createAsyncThunk(
     }
   }
 );
-
-
-export const registerUser = createAsyncThunk(
-  "auth/registerUser",
-  async (userData, thunkAPI) => {
-    const fakeToken =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3NDA3NTkwNTAsImV4cCI6MTc3MjI5NTA1OCwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsImlkIjoiMzQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20ifQ.TleIsUJCWQHyRJpv6xZb25LWmVGGkJtb5n663eILKgs";
-
-    const decoded = decodeJWT(fakeToken);
-    if (!decoded) {
-      return thunkAPI.rejectWithValue("Error al procesar el token");
-    }
-
-    const newUser = {
-      id: decoded.id,
-      name: userData.name,
-      username: userData.username,
-    };
-
-    localStorage.setItem("token", fakeToken);
-    localStorage.setItem("user", JSON.stringify(newUser));
-
-    return { token: fakeToken, user: newUser };
-  }
-);
