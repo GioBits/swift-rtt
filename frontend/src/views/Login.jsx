41c078ed9c5573263aa3a3d5df52a078fe692b00
Sergio Carrillo <sergiocarrillo_@outlook.com>
2025-03-13 23:00:17 -0400

    RTT-154 feat<frontend>: Responsive with breakpoints on Login View

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { LoginForm } from '../components/forms/LoginForm';
 import { RegisterForm } from '../components/forms/RegisterForm'
 import { useLocation } from 'react-router-dom';
-import logoColibri from '../assets/logo_colibri.png';
 
 const Login = () => {
   const location = useLocation();
@@ -13,25 +12,28 @@ const Login = () => {
 
   return (
     <>
-      <div className='flex flex-col items-center justify-center h-screen w-full bg-[#1c6e8c] z-10'>
-        <div className='flex flex-col items-center justify-center h-screen w-full -mt-30'>
-          <img src={logoColibri} alt='Colibri Logo' className='h-[15%] w-[15%] mb-4' />
-          <div className='relative mb-12 text-center'>
-            <h1 className='text-white text-6xl mb-1 font-serif font-bold'>SwiftVoice</h1>
-            <h2 className='text-white text-xl uppercase font-sans font-semibold'>
+      <div className='flex flex-col items-center justify-center h-screen w-screen bg-[#1c6e8c]'>
+        <div className='flex flex-col h-full w-full p-6 m-auto z-10 gap-6'>
+          {/* Title */}
+          <div className='flex flex-col text-center'>
+            <h1 className='text-white text-5xl md:text-6xl mb-1 font-serif font-bold'>SwiftVoice</h1>
+            <h2 className='text-white text-lg md:text-xl uppercase font-sans font-semibold'>
               TRANSCRIPCIÓN - TRADUCCIÓN - TEXTO A VOZ
             </h2>
           </div>
-
-          {location.pathname === '/register' ? < RegisterForm /> : < LoginForm />}
-
-          <footer className='absolute bottom-0 w-full text-white text-center py-4'>
-            <div className='flex flex-row items-center justify-center mb-2 relative'>
-              <p className='text-base'>Desarrollado por <strong>Bug Buster</strong></p>
+          {/* Forms and path */}
+          <div className='flex mx-auto z-10'>
+            <div className='flex flex-row w-100 md:w-200 h-100 rounded-lg bg-white'>
+            {location.pathname === '/register' ? < RegisterForm /> : < LoginForm />}
+            </div>
+          </div>
+        </div>
+          <footer className='flex flex-col w-screen text-white text-center py-2 bg-brownie z-1'>
+            <div className='flex flex-row items-center justify-center mb-1'>
+              <p className='text-base'>Desarrollado por <strong>BugBuster</strong></p>
             </div>
             <span className='text-sm'>&copy; Enero - Marzo 2025 <strong>BugBuster&trade;</strong></span>
           </footer>
-        </div>
       </div>
     </>
   );
