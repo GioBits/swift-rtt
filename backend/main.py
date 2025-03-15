6170b8d03e5f251ce68a96cc704dca39a0a2b7be
abelzavaleta <abel@origin.63>
2025-03-15 17:35:25 -0400

    HF ALLOWED CORS

--- backend/main.py
+++ backend/main.py
@@ -80,7 +80,7 @@ app.include_router(utilsRouter, prefix=("/utils"))
 allowed_origins = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://66.42.83.237:3000"],
+    allow_origins=["http://82.86.111.16", "http://localhost", "http://66.42.83.237", "http://138.199.35.206"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
