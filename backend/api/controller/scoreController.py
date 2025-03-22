34caae52ca86f701b7fd20164afd1bc38854f545
JesusPrieto18 <jlpt.1910@gmail.com>
2025-03-22 13:26:49 -0400

    RTT-166 feat<BACKEND>: create get_current_user_score controller

--- backend/api/controller/scoreController.py
+++ backend/api/controller/scoreController.py
@@ -1,12 +1,15 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 from api.service.scoreService import ScoreService
+from api.controller.userController import userController
+from utils.auth import AuthUtils
 #from schemas.score_schema import ScoresSchema
 
 class ScoreController:
     def __init__(self):
         self.score_service = ScoreService()
-
+        self.user_controller = userController()
+        self.auth = AuthUtils()
     async def get_top_scores(self, n: int):
         """
         Controlador que obtiene los N scores más altos.
@@ -27,12 +30,12 @@ class ScoreController:
                 detail="Internal Server Error"
             )
         
-    async def get_score_by_user(user_id: int):
+    async def get_score_by_user(self, user_id: int):
         """
         Controlador que maneja la lógica para obtener el score de un usuario por su ID.
         """
         try:
-            score = get_score_by_user_id(user_id)
+            score = self.score_service.get_score_by_user_id(user_id)
             if not score:
                 raise HTTPException(status_code=404, detail="Score no encontrado para el usuario")
             return score
@@ -45,3 +48,15 @@ class ScoreController:
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="Internal Server Error"
             )
+        
+    async def get_current_user_score(self, session_token: str):
+        """
+        Obtains the information of the authenticated user from the token in the cookie.
+        """
+        try:
+            user = self.user_controller.get_current_user(session_token)
+            id = user.get("id")
+            score = self.score_service(id)
+            return score
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
