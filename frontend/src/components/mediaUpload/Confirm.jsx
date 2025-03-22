ec7c40a737638b2493e0598c0bbf3497c914514d
Sergio Carrillo <sergiocarrillo_@outlook.com>
2025-03-22 17:23:08 -0400

    RTT-162 feat<frontend>:  implement centralized step management

--- frontend/src/components/mediaUpload/Confirm.jsx
+++ frontend/src/components/mediaUpload/Confirm.jsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import PropTypes from "prop-types";
 import AudioService from '../../service/audioService';
 import { MediaContext } from '../../contexts/MediaContext';
+import { StepperStep } from "../../constants/stepper";
 
 const Confirm = ({ file, audioId, handleNewAudio }) => {
   const {
@@ -23,7 +24,7 @@ const Confirm = ({ file, audioId, handleNewAudio }) => {
       return;
     }
 
-    resetStepper(2);
+    resetStepper(StepperStep.UPLOAD);
     try {
       if (audioId) {
         await processMedia();
@@ -50,7 +51,7 @@ const Confirm = ({ file, audioId, handleNewAudio }) => {
       id: response.id.toString()
     });
 
-    setCurrentStep(2);
+    setCurrentStep(StepperStep.UPLOAD);
     setIsUploading(false);
     console.log("Audio uploaded"); // Log the processing response
   };
@@ -67,7 +68,7 @@ const Confirm = ({ file, audioId, handleNewAudio }) => {
       selectedLanguages
     );
 
-    setCurrentStep(3);
+    setCurrentStep(StepperStep.PROCESSING);
     console.log("Audio processing", processResponse); // Log the processing response
   };
 
