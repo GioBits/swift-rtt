77db8722f1f37f6d9f95281f4cc6d00d5729c06f
JesusPrieto18 <jlpt.1910@gmail.com>
2025-03-22 13:25:30 -0400

    RTT-166 feat<BACKEND>: create route get_current_user_score

--- backend/api/routes/scoreRoute.py
+++ backend/api/routes/scoreRoute.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, Cookie
 from sqlalchemy.orm import Session
 from db.database import get_db
 from api.controller.scoreController import ScoreController 
@@ -7,11 +7,18 @@ from models.scores import ScoresSchema
 score_controller = ScoreController()
 router = APIRouter(prefix="/scores", tags=["Scores"])
 
-@router.get("/top", response_model=list[ScoresSchema])
+@router.get("/top", response_model=list[ScoresSchema], tags=["Scores"])
 async def get_top_scores(n: int = Query(..., description="Cantidad de scores a mostrar")):
     return await score_controller.get_top_scores(n)
 
-@router.get("/user/{user_id}", response_model=ScoresSchema)
+@router.get("/user/me", tags=["Scores"])
+async def get_current_user_score(session_token: str = Cookie(None)):
+    """
+    Obtiene la información del usuario autenticado a partir del token en la cookie.
+    """
+    return await score_controller.get_current_user_score(session_token)
+
+@router.get("/user/{user_id}", response_model=ScoresSchema, tags=["Scores"])
 async def get_user_score(user_id: int):
     """
     Endpoint para obtener el score de un usuario específico.
